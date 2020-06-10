28fe190f919f3169a1115fc965eaa0ebd6160e72
13031042412 <13031042412@163.com>
2020-06-10 17:46:23 +0800

    增加富文本编辑器

---
+++ module-vue2editor/README.md
@@ -0,0 +1,57 @@
+# module-vue2editor
+
+## 安装
+```
+npm install vue2-editor
+```
+
+## 使用
+```
+import { VueEditor } from "vue2-editor";
+
+<vue-editor
+    id="editor"
+    v-model="content"
+    placeholder="请编辑内容"
+/>
+```
+
+## toolbar
+```
+var fullToolbar = [
+  [{ font: [] }],
+
+  [{ header: [false, 1, 2, 3, 4, 5, 6] }],
+
+  [{ size: ["small", false, "large", "huge"] }],
+
+  ["bold", "italic", "underline", "strike"],
+
+  [
+    { align: "" },
+    { align: "center" },
+    { align: "right" },
+    { align: "justify" }
+  ],
+
+  [{ header: 1 }, { header: 2 }],
+
+  ["blockquote", "code-block"],
+
+  [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+
+  [{ script: "sub" }, { script: "super" }],
+
+  [{ indent: "-1" }, { indent: "+1" }],
+
+  [{ color: [] }, { background: [] }],
+
+  ["link", "image", "video", "formula"],
+
+  [{ direction: "rtl" }],
+  
+  ["clean"]
+];
+```
+
+
---
+++ module-vue2editor/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="hello">
+        <vue-editor
+            id="editor"
+            v-model="content"
+            placeholder="请编辑内容"
+            :editor-toolbar="customToolbar"
+        />
+    </div>
+</template>
+
+<script>
+import { VueEditor } from "vue2-editor";
+export default {
+    name: "HelloWorld",
+    props: {
+        msg: String
+    },
+    components: { VueEditor },
+    data() {
+        return {
+            content: "",
+            customToolbar: [
+                [{ header: [false, 1, 2, 3, 4, 5, 6] }],
+                ["bold", "italic", "underline", "strike"],
+                [{ color: [] }, { background: [] }],
+                [
+                    { align: "" },
+                    { align: "center" },
+                    { align: "right" },
+                    { align: "justify" }
+                ],
+                [{ indent: "-1" }, { indent: "+1" }],
+                [{ list: "ordered" }, { list: "bullet" }],
+                ["blockquote", "code-block"],
+                ["link", "image"]
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+.hello {
+    max-width: 800px;
+    margin: 0 auto;
+}
+</style>
